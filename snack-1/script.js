8419af8d2637358e13386ad66b3f82ae36ab49e3
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-21 16:45:57 +0100

    snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,4 +2,4 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
-console.log(`${names}`);
+console.log(`'${names}'`);
