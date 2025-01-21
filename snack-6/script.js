7e8f5b237668a57d676713e779878cc5e66a1fb2
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-21 18:16:46 +0100

    snack-6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,18 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+
+
+const xxlZucchine = zucchine.filter((zucchina) =>{
+  return zucchina.length >= 15
+});
+
+// console.log(xxlZucchine);
+
+const xxsZucchine = zucchine.filter((zucchina) =>{
+  return zucchina.length < 15
+});
+
+console.log(xxsZucchine);
+
