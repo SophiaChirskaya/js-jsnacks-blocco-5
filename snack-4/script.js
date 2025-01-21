27acc1c595e0430a9d414ff8968e73c2392db322
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-21 17:53:16 +0100

    snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -3,7 +3,13 @@ const posts = [
   { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
-]
+];
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorPost = posts.map((post) =>{
+  return post.author
+});
+
+console.log(authorPost);
